357e2d3244a69c66aa0c1aa43f83ef10544ebeba
Steven-Jarmell <sjj27@pitt.edu>
2023-02-02 11:18:23 -0500

    Update call to server within github login component

--- client/src/components/header/GitHubLogin.tsx
+++ client/src/components/header/GitHubLogin.tsx
@@ -55,7 +55,7 @@ const GitHubLogin = (): JSX.Element => {
 
     // Get the user's data from the GitHub API
     async function getUserData() {
-        await fetch("http://localhost:5000/auth/getUserData", {
+        await fetch("https://pittcsc-api/auth/getUserData", {
             method: "GET",
             headers: {
                 "Authorization": "Bearer " + localStorage.getItem("accessToken"), // Make sure "Authorization" is in quotes
@@ -72,7 +72,7 @@ const GitHubLogin = (): JSX.Element => {
     // Whenever userData is retrieved, get the user's roles from the database
     useEffect(() => {
         async function getUserRoles() {
-            await fetch(`http://localhost:5000/user?id=${userData.id}`, {
+            await fetch(`https://pittcsc-api/user?id=${userData.id}`, {
                 method: "GET",
             })
                 .then((res) => {
@@ -113,7 +113,7 @@ const GitHubLogin = (): JSX.Element => {
     useEffect(() => {
         async function getGitHubClientID() {
             const fetchedGithubClientId = await fetch(
-                "http://localhost:5000/auth/githubClientID"
+                "https://pittcsc-api/auth/githubClientID"
             ).then((res) => res.json());
             setGithubClientId(fetchedGithubClientId);
         }
@@ -132,7 +132,7 @@ const GitHubLogin = (): JSX.Element => {
         if (codeParam && localStorage.getItem("accessToken") === null) {
             async function getAccessToken() {
                 await fetch(
-                    `http://localhost:5000/auth/getAccessToken?code=${codeParam}`,
+                    `https://pittcsc-api/auth/getAccessToken?code=${codeParam}`,
                     {
                         method: "GET",
                     }
