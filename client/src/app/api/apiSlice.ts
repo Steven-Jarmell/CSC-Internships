65a2512a9a1fd6a64f5e1da19fcb2d1f5f1c4323
Steven-Jarmell <sjj27@pitt.edu>
2023-02-07 18:06:26 -0500

    Switch URLs back to hosted backend in preparation for push to production

--- client/src/app/api/apiSlice.ts
+++ client/src/app/api/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define the base query for the API
 const baseQuery = fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: "https://pittcsc-api.onrender.com",
 });
 
 // Define the API endpoints
